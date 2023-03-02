0b46fc6cb1fd3a6343bc996b7a77b8bbbdcbed17
soubhagyasethy <ssethy017@gmail.com>
2023-03-02 23:45:51 +0530

    feat: add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import PostDetail from "./PostDetail";
+import CreatePost from "./CreatePost";
+
 function App() {
   return (
-    <div className="App">
-      <h1>WriteUpNow</h1>
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
+        <Route path="/create-Post" element={<CreatePost />} />
+      </Routes>
     </div>
   );
 }
