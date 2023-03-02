490043a2a79b7e8dd90eca36e0aa11e853603127
soubhagyasethy <ssethy017@gmail.com>
2023-03-03 01:36:44 +0530

    feat: updated form input of create post page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,22 +1,45 @@
+import { useState } from "react";
+
 function CreatePost() {
+  const [title, setTitle] = useState();
+  const [subtitle, setSubTitle] = useState();
+  const [content, setContent] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("title: ", title);
+    console.log("subtitle: ", subtitle);
+    console.log("content: ", content);
+  }
   return (
     <div className="create-post">
       <h1>Create Post</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
 
         <div className="form-field">
           <label>Sub Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={subtitle}
+            onChange={(e) => setSubTitle(e.target.value)}
+          />
         </div>
 
         <div className="form-field">
           <label>Content</label>
-          <textarea></textarea>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
         </div>
 
         <button className="create-post-button">Create Post</button>
