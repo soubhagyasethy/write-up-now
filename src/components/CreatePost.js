e981b5603e431255a88ae11428f1effea55a907f
soubhagyasethy <ssethy017@gmail.com>
2023-03-03 00:56:13 +0530

    feat: updated create post page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,28 @@
 function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+
+      <form>
+        <div className="form-field">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-field">
+          <label>Sub Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-field">
+          <label>Content</label>
+          <textarea></textarea>
+        </div>
+
+        <button className="create-post-button">Create Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
